af5635858f644dd52ae027bde78aea4290796f4c
SaiTeja-Ambari <saiteja.ambari@gmail.com>
2022-04-04 09:58:24 +0530

    twoSum

---
+++ python/twoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        n=len(nums)
+        for i in range(n):
+            if(target-nums[i] in d):
+                return([d[target-nums[i]],i])
+            d[nums[i]]=i
+            
